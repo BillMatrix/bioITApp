331b3a4d426f28510fffef598831626d4cdf86b9
Bill Zhu <zheqing.zhu@duke.edu>
2015-10-15 13:09:00 +0000

    Add HelloWorld

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def hello
+    print "Hello World!"
+  end
 end
